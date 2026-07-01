ebdd39382ab119cd7e75d2b0167886df0d750384
sameer9860 <samirkhatiwada68@gmail.com>
2026-07-01 20:37:45 +0545

    feat : project setup

---
+++ backend/smartbill/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for smartbill project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smartbill.settings')
+
+application = get_asgi_application()
