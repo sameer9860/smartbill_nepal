3ebb0926bc10fcb0f8ac53dfbd605601c4360ff0
sameer9860 <samirkhatiwada68@gmail.com>
2026-08-16 21:03:29 +0545

    Feat : add comment

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -4,11 +4,16 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    # Auth
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
 
+    # Profile
     path('profile/', views.profile, name='profile'),
-    
+
+    # Change password
     path('change-password/', views.change_password, name='change_password'),
+
+    # Delete account
     path('delete-account/', views.delete_account, name='delete_account'),
 ]
