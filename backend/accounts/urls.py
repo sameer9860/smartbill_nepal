99333904136ba1161886f9de8eb8d8a83f972b4f
sameer9860 <samirkhatiwada68@gmail.com>
2026-08-16 20:59:44 +0545

    Feat : update account urls

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -6,8 +6,9 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
-    path('profile/', views.profile, name='profile'),
-    path('change-password/', views.change_password, name='change_password'),
 
+    path('profile/', views.profile, name='profile'),
+    
+    path('change-password/', views.change_password, name='change_password'),
     path('delete-account/', views.delete_account, name='delete_account'),
 ]
